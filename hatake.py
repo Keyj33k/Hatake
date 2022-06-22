5ae7cb2cffde0d6093a831a4f2af498d982419b3
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-06-22 16:17:25 +0200

    Update hatake.py

--- hatake.py
+++ hatake.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 import sys, getopt
-import requests
+try:
+   import requests
+except ImportError:
+   raise RuntimeError("Failed to import requests module. EXIT.")
 
 # # # # # # # # # # # # # # # # # # # # # #
 #                                         #
@@ -18,50 +21,45 @@ def help_func():
 \nUsage:
 \033[0;31m------\033[0;37m
    \033[0;33m>>\033[0;37m python3 hatake\033[0;31m.\033[0;37mpy \033[0;31m-\033[0;37ma\033[0;31m/--\033[0;37mipa \033[0;31m<\033[0;37mIPV4\033[0;31m> \033[0;31m|\033[0;37m get information about a given IPv4 address
+   \033[0;33m>>\033[0;37m python3 hatake\033[0;31m.\033[0;37mpy \033[0;31m-\033[0;37mp\033[0;31m/--\033[0;37mpipa \033[0;31m|\033[0;37m get your public ipv4 address
 \nOptional:
 \033[0;31m---------\033[0;37m
    \033[0;33m>>\033[0;37m python3 hatake\033[0;31m.\033[0;37mpy \033[0;31m-\033[0;37mh\033[0;31m/--\033[0;37mhelp \033[0;31m|\033[0;37m show help page
    """)
 
-def hatake(argv):
-   ipver4 = ""
+def public_ipv4_request():
+   print(
+      f"\033[0;37m[\033[0;31m*\033[0;37m] Written by \033[0;31m@\033[0;37mKeyj33k\n" +
+      f"\033[0;37m[\033[0;31m+\033[0;37m] Starting Hatake at {datetime.now()}\n" +
+      f"\033[0;33m=" * 50
+   )
 
    try:
-      opts, args = getopt.getopt(argv, "ha:", ["ipa="])
-   except getopt.GetoptError:
-      print(f"\033[0;37m[\033[0;31m*\033[0;37m] Usage\033[0;31m:\033[0;37m python3 hatake\033[0;31m.\033[0;37mpy \033[0;31m-\033[0;37ma \033[0;31m<\033[0;37mIPV4\033[0;31m>")
-      sys.exit(1)
+      print(f"\033[0;37m[\033[0;31m+\033[0;37m] Your Public IPv4\033[0;31m:\033[0;37m\t{requests.get('https://api.ipify.org').text}")
+   except requests.RequestException as reqexc:
+      print(reqexc)
 
-   for opt, arg in opts:
-      if opt == '-h' or opt == '--help':
-         help_func()
-         sys.exit(0)
-      elif opt in ("-a", "--ipa"):
-         ipver4 = arg
-      else:
-         raise getopt.GetoptError(
-            f"\033[0;37m[\033[0;31m*\033[0;37m] Usage\033[0;31m:\033[0;37m python3 hatake\033[0;31m.\033[0;37mpy \033[0;31m-\033[0;37ma \033[0;31m<\033[0;37mIPV4\033[0;31m>"
-         )
-
+def api_request(ipver4):
    tstart = datetime.now()
 
    print(
       f"\033[0;37m[\033[0;31m*\033[0;37m] Written by \033[0;31m@\033[0;37mKeyj33k\n" +
-      f"\033[0;37m[\033[0;31m*\033[0;37m] IPv4\033[0;31m-\033[0;37mLocator \033[0;31m|\033[0;37m Ver\033[0;31m.:\033[0;37m1\033[0;31m.\033[0;37m0\033[0;31m.\033[0;37m0\n" +
       f"\033[0;37m[\033[0;31m+\033[0;37m] Starting Hatake at {datetime.now()}\n" +
       f"\033[0;33m=" * 50
    )
 
    try:
       if len(ipver4) == 0:
-         print(f"\033[0;37m[\033[0;31m*\033[0;37m] Usage\033[0;31m:\033[0;37m python3 hatake\033[0;31m.\033[0;37mpy \033[0;31m-\033[0;37ma \033[0;31m<\033[0;37mIPV4\033[0;31m>")
+         print(
+            f"\033[0;37m[\033[0;31m*\033[0;37m] Usage\033[0;31m:\033[0;37m python3 hatake\033[0;31m.\033[0;37mpy \033[0;31m-\033[0;37mh"
+         )
          sys.exit(1)
 
       response = requests.get(
          f"""http://ip-api.com/json/{ipver4}?fields=status,message,continent,
-         continentCode,country,countryCode,region,regionName,city,district,
-         zip,lat,lon,timezone,currency,isp,org,as,asname,reverse,mobile,
-         proxy,hosting,query"""
+            continentCode,country,countryCode,region,regionName,city,district,
+            zip,lat,lon,timezone,currency,isp,org,as,asname,reverse,mobile,
+            proxy,hosting,query"""
       ).json()
 
       lst = list(response.items())
@@ -104,6 +102,32 @@ def hatake(argv):
       f"\n\033[0;37m[\033[0;31m+\033[0;37m] Hatake done in {tend - tstart}"
    )
 
+def hatake(argv):
+   try:
+      # opts, args = getopt.getopt(argv, "ha:p", ["ipa=", "pipa="])
+      opts, args = getopt.getopt(argv, "ha:ph", ["addr=", "pipa", "help"])
+   except getopt.GetoptError:
+      print(f"\033[0;37m[\033[0;31m*\033[0;37m] Usage\033[0;31m:\033[0;37m python3 hatake\033[0;31m.\033[0;37mpy \033[0;31m-\033[0;37mh")
+      sys.exit(1)
+
+   for opt, arg in opts:
+      if opt == '-h' or opt == '--help':
+         help_func()
+         sys.exit(0)
+      elif opt in ("-a", "--addr"):
+         ipver4 = arg
+         if len(ipver4) == 0:
+            print(f"\033[0;37m[\033[0;31m*\033[0;37m] Usage\033[0;31m:\033[0;37m python3 hatake\033[0;31m.\033[0;37mpy \033[0;31m-\033[0;37mh")
+            sys.exit(1)
+         api_request(ipver4)
+      elif opt in ("-p", "--pipa"):
+         public_ipv4_request()
+         sys.exit(0)
+      else:
+         raise getopt.GetoptError(
+            f"\033[0;37m[\033[0;31m*\033[0;37m] Usage\033[0;31m:\033[0;37m python3 hatake\033[0;31m.\033[0;37mpy \033[0;31m-\033[0;37mh"
+         )
+
 
 if __name__ == "__main__":
    from termcolor import colored
@@ -116,7 +140,7 @@ if __name__ == "__main__":
             "hatake",
             font='bulbhead'
          ), "red"
-      )
+      ), f"\n\033[0;37m[\033[0;31m*\033[0;37m] IPv4\033[0;31m-\033[0;37mLocator \033[0;31m|\033[0;37m Ver\033[0;31m.:\033[0;37m1\033[0;31m.\033[0;37m0\033[0;31m.\033[0;37m0"
    )
 
    hatake(sys.argv[1:])
